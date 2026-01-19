464e8c72891768a060d6c86ac9a290095dcc51f5
mrisqikhasani <mrisqikhasani@gmail.com>
2026-01-19 21:23:20 +0700

    initial sliver-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:practice_class/model/tourism.dart';
-import 'package:practice_class/screen/detail/detail_screen.dart';
-import 'package:practice_class/screen/home/home_screen.dart';
-import 'package:practice_class/static/navigation_route.dart';
-import 'package:practice_class/style/theme/tourism_theme.dart';
-
+ 
+import 'package:practice_class/learning_path_screen.dart';
+ 
 void main() {
-  runApp(const MyApp());
+ runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: TourismTheme.lightTheme,
-      darkTheme: TourismTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      // home: const HomeScreen(),
-      initialRoute: NavigationRoute.homeRoute.name,
-      routes: {
-        NavigationRoute.homeRoute.name: (context) => const HomeScreen(),
-        NavigationRoute.detailRoute.name: (context) => DetailScreen(
-          tourism: ModalRoute.of(context)?.settings.arguments as Tourism,
-        ),
-      },
-    );
-  }
+ const MyApp({super.key});
+ 
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       useMaterial3: true,
+     ),
+     home: const LearningPathScreen(),
+   );
+ }
 }
--- lib/static/navigation_route.dart
+++
@@ -1,7 +0,0 @@
-enum NavigationRoute {
- homeRoute("/"),
- detailRoute("/detail");
- 
- const NavigationRoute(this.name);
- final String name;
-}
